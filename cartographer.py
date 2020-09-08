ce7a1b1fd293c3aace1339e5e325a99161eec19f
Miles <miles-moran@hotmail.com>
2020-09-08 17:12:25 -0500

    started the flighpath building application.

---
+++ cartographer.py
@@ -0,0 +1,110 @@
+from helpers import getTree
+from pprint import pprint
+import copy
+# def method(tree, sample):
+#     keys = sample.keys()
+#     pprint(keys)
+#     routine = {}
+#     for field in keys:
+#         x = '//*[(text() = "{}")]'.format(sample[field])
+#         elements = tree.xpath(x)
+#         if len(elements) == 0:
+#             print("field '{}' not found".format(field))
+#             return None
+#         element = elements[0]
+#         tag = element.tag
+#         className = element.get('class')
+#         xpath = "//{}[@class='{}']".format(tag, className)
+#         routine[field] = {
+#             "path": xpath
+#         }
+#     return routine
+
+def cartograph(desired):
+    def explore(des, tree):
+        pprint(des)
+        goal = copy.deepcopy(des)
+        keys = goal.keys()
+        for key in keys:
+            obj = goal[key]
+            typeOfObj = type(obj)
+            if typeOfObj == dict:
+                continue
+            if typeOfObj == list:
+                profile = obj[0]
+                typeOfSub = type(profile)
+                if typeOfSub == str:
+                    test = {key: profile}
+                    test = explore(test, tree)
+                    goal[key] = [test[key]]
+                if typeOfSub == dict:
+                    expander = None
+                    subK = list(profile.keys())
+                    subKeys = list(profile.values())
+                    fields = {}
+                    for s in range(0, len(subKeys)):
+                        text = subKeys[s]
+                        parent = None
+                        if s == 0:
+                            expander = findXpath(tree, text)
+                            fields[subK[s]] = getUniqueXpath(tree.xpath(expander)[0])
+                        else:
+                            while True:
+                                parent = tree.xpath(expander + "/parent::*")
+                                parent = parent[0]
+                                parentPath = getUniqueXpath(parent)
+                                xpath = "{}/*[contains(text(), '{}')]".format(parentPath, text)
+                                pprint(xpath)
+                                elements = tree.xpath(xpath)
+                                if len(elements) != 0:
+                                    path = getUniqueXpath(elements[0])
+                                    fields[subK[s]] = path
+                                    expander = parentPath
+                                    break
+                    fields["iterate"] = expander
+                    goal[key] = [fields]                        
+                continue
+            if typeOfObj == str:
+                xpath = findXpath(tree, obj)
+                goal[key] = xpath
+                continue
+        return goal
+    tree = getTree("https://www.indeed.com/jobs?q=sales&l&vjk=882afa687e67339e")
+    result = explore(desired, tree)
+    print('-------RESULT-------')
+    pprint(result)
+    
+def findXpath(tree, text):
+    x = '//*[contains(text(), "{}")]'.format(text)
+    elements = tree.xpath(x)
+    element = elements[0]
+    xpath = getUniqueXpath(element)
+    return xpath
+
+def getUniqueXpath(element):
+    tag = element.tag
+    className = element.get('class')
+    xpath = "//{}[@class='{}']".format(tag, className)
+    return xpath
+
+
+# desired = {
+#     "header": "Jobs in St. Louis, Missouri",
+#     "titles": ["Senior Software Dev"],
+#     "jobs": [{
+#         "title": "Senior Software Dev",
+#         "description": "Need a master of React Native, a man or woman with gumption, who can lead a team.",
+#     }]
+# }
+
+
+desired = {
+    "header": "sales jobs",
+    "titles": ["Solar Sales Specialist"],
+    "jobs": [{
+        "title": "Solar Sales Specialist",
+        "description": "Bella Villa, MO",
+    }]
+}
+
+cartograph(desired)
---
+++ helpers.py
@@ -0,0 +1,18 @@
+from lxml import html
+import requests
+import copy
+
+def getTree(URL):
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
+        'Accept-Language': 'en-gb',
+        'Accept-Encoding': 'br, gzip, deflate',
+        'Accept': 'test/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    }
+    try:
+        r = requests.get(URL, headers=headers, timeout=10)
+        tree = html.fromstring(r.content.decode("utf-8", "replace"))
+        return tree
+    except Exception as e:
+        print(e)
+        return None
